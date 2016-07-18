5ba50c932e7a615cb6a8857aa7cdbd4475b5d671
Moritz Lotze <lotzemoritz@gmail.com>
2016-07-18 17:02:18 +0200

    add example docstring

--- honda_flux.py
+++ honda_flux.py
@@ -3,6 +3,22 @@ import numpy as np
 
 
 class HondaFlux(object):
+    """
+    Get Honda 2015 atmospheric neutrino fluxes.
+
+    >>> from honda_flux import HondaFlux
+    >>> flux = HondaFlux('file_with_fluxtables.h5')
+
+    >>> zen = np.linspace(0, np.pi, 11)
+    >>> ene = np.logspace(0, 2, 11)
+
+    >>> flux('nu_e_bar', zen, ene)
+    array([  6.68440000e+01,   1.83370000e+01,   4.96390000e+00,
+         1.61780000e+00,   5.05350000e-01,   2.29920000e-01,
+         2.34160000e-02,   2.99460000e-03,   3.77690000e-04,
+         6.87310000e-05,   1.42550000e-05])
+
+    """
     def __init__(self, filename):
         self.tables = {}
         with h5py.File(filename, 'r') as h5:
