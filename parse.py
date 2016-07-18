671d0ff20835616682ea9a2d8abc9fa7a1782071
Moritz Lotze <lotzemoritz@gmail.com>
2016-07-18 14:49:46 +0200

    cleanup, add energy + zenith arrays

--- parse.py
+++ parse.py
@@ -14,11 +14,13 @@ zen = np.linspace(-1.0, 1.0, nbins + 1)
 zen_binsize = 2.0/nbins
 zen_low = zen[:-1]
 zen_hi = zen[1:]
+zenith = zen_low + 0.5 * zen_binsize
 bins = np.column_stack((zen_low, zen_hi))
 
+prefix = '/home/mlotze/dev/honda_2015'
 dfs = []
 for lo, hi in bins:
-    fname = 'cos_%s_%s.csv' % (str(lo), str(hi))
+    fname = prefix + '/' + 'cos_%s_%s.csv' % (str(lo), str(hi))
     df = pd.read_table(fname, header=1, delim_whitespace=True)
     n_evts = len(df)
     #df['cos_low'] = np.ones(n_evts, float) * lo
@@ -27,7 +29,14 @@ for lo, hi in bins:
     dfs.append(df)
 
 df = pd.concat(dfs)
+energy = np.unique(df['Enu'])
+df = df.set_index(['cos', 'Enu'])
 
+# (Energy, cos) matrices
+numu = df['NuMu'].unstack().values
+numubar = df['NuMubar'].unstack().values
+nue = df['NuE'].unstack().values
+nuebar = df['NuEbar'].unstack().values
 
 #rec = df.to_records()
 #h5 = h5py.File('honda2015_frejus_solarmin.h5')
