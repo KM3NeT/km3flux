9e08d697180b8bff0897bec7e097a31cd62369d6
Moritz Lotze <lotzemoritz@gmail.com>
2016-07-18 15:01:32 +0200

    its cos(zen), not zen, stupid!

--- parse.py
+++ parse.py
@@ -8,14 +8,14 @@ import numpy as np
 import pandas as pd
 
 
-# define zenith bins
+# define coszenith bins
 nbins = 20
-zen = np.linspace(-1.0, 1.0, nbins + 1)
-zen_binsize = 2.0/nbins
-zen_low = zen[:-1]
-zen_hi = zen[1:]
-zenith = zen_low + 0.5 * zen_binsize
-bins = np.column_stack((zen_low, zen_hi))
+cos_binlims = np.linspace(-1.0, 1.0, nbins + 1)
+coszen_binsize = 2.0/nbins
+coszen_low = cos_binlims[:-1]
+coszen_hi = cos_binlims[1:]
+cos_zen = coszen_low + 0.5 * coszen_binsize
+bins = np.column_stack((coszen_low, coszen_hi))
 
 prefix = '/home/mlotze/dev/honda_2015'
 dfs = []
@@ -23,14 +23,12 @@ for lo, hi in bins:
     fname = prefix + '/' + 'cos_%s_%s.csv' % (str(lo), str(hi))
     df = pd.read_table(fname, header=1, delim_whitespace=True)
     n_evts = len(df)
-    #df['cos_low'] = np.ones(n_evts, float) * lo
-    #df['cos_high'] = np.ones(n_evts, float) * hi
-    df['cos'] = lo + 0.5 * zen_binsize
+    df['cos_zen'] = lo + 0.5 * coszen_binsize
     dfs.append(df)
 
 df = pd.concat(dfs)
 energy = np.unique(df['Enu'])
-df = df.set_index(['cos', 'Enu'])
+df = df.set_index(['cos_zen', 'Enu'])
 
 # (Energy, cos) matrices
 numu = df['NuMu'].unstack().values
