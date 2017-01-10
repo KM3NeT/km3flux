9f96f2a082fc6fd99bc215823256360426057811
Moritz Lotze <lotzemoritz@gmail.com>
2017-01-10 18:57:20 +0100

    bump version

--- honda_2015/__init__.py
+++ honda_2015/__init__.py
@@ -2,4 +2,4 @@ from honda_2015.honda_flux import HondaFlux
 from honda_2015.honda_pandas import honda_df
 from honda_2015.honda_kpmod import HondaMod
 
-__version__ = '0.3'
+__version__ = '0.4'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='honda_2015',
-    version='0.2',
+    version='0.4',
     description='Atmospheric Neutrino Fluxes',
     url='http://git.km3net.de/moritz/honda_2015',
     author='Moritz Lotze',
