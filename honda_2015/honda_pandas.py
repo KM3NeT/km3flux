fbe75cbfd4eae010b2fd893f235b03b6be31d92d
Moritz Lotze <lotzemoritz@gmail.com>
2017-02-21 15:57:02 +0100

    fix dtype

--- honda_2015/honda_pandas.py
+++ honda_2015/honda_pandas.py
@@ -5,7 +5,7 @@ from honda_2015 import HondaFlux, HondaSarcevic
 
 
 def honda_df(df, average=True):
-    out = np.zeros_like(df.flavor)
+    out = np.zeros(len(df.flavor))
     hf = HondaFlux()
     for flav in {'nu_mu', 'anu_mu', 'nu_e', 'anu_e'}:       # noqa
         mask = np.array(df.flavor == flav, dtype=bool)
@@ -19,7 +19,7 @@ def honda_df(df, average=True):
 
 
 def honda_sarcevic_df(df):
-    out = np.zeros_like(df.flavor)
+    out = np.zeros(len(df.flavor))
     hf = HondaFlux()
     for flav in {'nu_mu', 'anu_mu', 'nu_e', 'anu_e'}:       # noqa
         mask = df.flavor == flav
