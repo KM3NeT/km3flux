84589849238e771db939546605e0d4923c4bc9d5
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-06 11:14:12 +0100

    optionally infer flavors/shape from dataframe

--- honda_2015/__init__.py
+++ honda_2015/__init__.py
@@ -1,5 +1,5 @@
 from honda_2015.honda_flux import HondaFlux, HondaSarcevic
-from honda_2015.honda_pandas import honda_df, honda_sarcevic_df
+from honda_2015.honda_pandas import honda_df
 from honda_2015.honda_kpmod import HondaMod, HondaSarcevicMod
 
 __version__ = '0.5'
--- honda_2015/honda_pandas.py
+++ honda_2015/honda_pandas.py
@@ -4,26 +4,19 @@ import numpy as np
 from honda_2015 import HondaFlux, HondaSarcevic
 
 
-def honda_df(df, average=True):
-    out = np.zeros(len(df.flavor))
+def honda_df(df, nevts=None, average=True, flavors=None):
+    if flavors is None:
+        flavors = {'nu_mu', 'anu_mu', 'nu_e', 'anu_e'}      # noqa
+    if nevts is None:
+        nevts = len(df)
+    out = np.zeros(nevts)
     hf = HondaFlux()
-    for flav in {'nu_mu', 'anu_mu', 'nu_e', 'anu_e'}:       # noqa
+    for flav in flavors:
         mask = np.array(df.flavor == flav, dtype=bool)
         if average:
             zen = None
         else:
             zen = df.zenith[mask]
-        buf = hf.get(flav, df.energy[mask], zen)
-        out[mask] = buf
-    return out
-
-
-def honda_sarcevic_df(df):
-    out = np.zeros(len(df.flavor))
-    hf = HondaFlux()
-    for flav in {'nu_mu', 'anu_mu', 'nu_e', 'anu_e'}:       # noqa
-        mask = df.flavor == flav
-        zen = df.zenith[mask]
         buf = hf.get(flav, df.energy[mask], zen)
         out[mask] = buf
     return out
