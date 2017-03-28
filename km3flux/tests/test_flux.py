1d2ae3183ab82abd564745073b954eacb42e2589
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-28 10:45:10 +0200

    add flux tests

--- km3flux/tests/test_flux.py
+++ km3flux/tests/test_flux.py
@@ -2,11 +2,98 @@ from unittest import TestCase
 
 import numpy as np
 
-from km3flux.flux import (Honda2015, HondaSarcevic, DarkMatterFlux)     # noqa
+from km3flux.flux import (BaseFlux, Honda2015, HondaSarcevic, DarkMatterFlux)     # noqa
 from km3flux.data import dm_gc_spectrum
 
 
-class TestDMSpectra(TestCase):
+class TestBaseFlux(TestCase):
+    def setUp(self):
+        self.flux = BaseFlux()
+        assert self.flux is not None
+
+    def test_shape_exception(self):
+        with self.assertRaises(ValueError):
+            self.flux([1, 2, 3], zenith=[3, 4])
+
+    def test_nonimplemented(self):
+        with self.assertRaises(NotImplementedError):
+            self.flux([1, 2, 3])
+
+        with self.assertRaises(NotImplementedError):
+            self.flux([1, 2, 3], zenith=[2, 3, 4])
+
+    def test_integration(self):
+        with self.assertRaises(NotImplementedError):
+            self.flux.integrate()
+        with self.assertRaises(NotImplementedError):
+            self.flux.integrate_samples([1, 2, 3])
+        with self.assertRaises(IndexError):
+            self.flux.integrate_samples([1, 2, 3], [1, 2])
+
+
+class TestHonda2015(TestCase):
+    def setUp(self):
+        self.flux = Honda2015()
+        assert self.flux is not None
+
+    def test_shape_exception(self):
+        with self.assertRaises(ValueError):
+            self.flux([1, 2, 3], zenith=[3, 4])
+
+    def test_call(self):
+        self.flux([1, 2, 3])
+        self.flux([1, 2, 3], zenith=[2, 3, 4])
+
+    def test_integration(self):
+        self.flux.integrate()
+        self.flux.integrate_samples([1, 2, 3])
+        with self.assertRaises(IndexError):
+            self.flux.integrate_samples([1, 2, 3], [1, 2])
+
+
+class TestHondaSarcevic(TestCase):
+    def setUp(self):
+        self.flux = HondaSarcevic()
+        assert self.flux is not None
+
+    def test_shape_exception(self):
+        with self.assertRaises(ValueError):
+            self.flux([1, 2, 3], zenith=[3, 4])
+
+    def test_call(self):
+        with self.assertRaises(NotImplementedError):
+            self.flux([1, 2, 3])
+        self.flux([1, 2, 3], zenith=[2, 3, 4])
+
+    def test_integration(self):
+        with self.assertRaises(NotImplementedError):
+            self.flux.integrate()
+        with self.assertRaises(NotImplementedError):
+            self.flux.integrate_samples([1, 2, 3])
+        with self.assertRaises(IndexError):
+            self.flux.integrate_samples([1, 2, 3], [1, 2])
+
+
+class TestDMFlux(TestCase):
+    def test_init(self):
+        dmf = DarkMatterFlux(flavor='nu_mu', channel='w', mass=90)
+        assert dmf is not None
+
+    def test_flux(self):
+        dmf = DarkMatterFlux(flavor='nu_mu', channel='w', mass=90)
+        dmf([3])
+        dmf([20])
+        with self.assertRaises(ValueError):
+            dmf([99])
+        dmf([18])
+        energy = np.array([3, 20, 18, 2, 4, 50, 28])
+        dmf(energy)
+        bad_energy = np.array([3, 20, 18, 2, 4, 50, 28, 99])
+        with self.assertRaises(ValueError):
+            dmf(bad_energy)
+
+
+class TestDartMatterLoader(TestCase):
     def test_cast(self):
         x1, y1 = dm_gc_spectrum('nu_mu', 'b', '90')
         x2, y2 = dm_gc_spectrum('nu_mu', 'b', 90)
@@ -28,22 +115,3 @@ class TestDMSpectra(TestCase):
             x, y = dm_gc_spectrum('nu_mu', 'crazy', '90')
         with self.assertRaises(KeyError):
             x, y = dm_gc_spectrum('nu_mu', 'b', '90000000000')
-
-
-class TestDMFlux(TestCase):
-    def test_init(self):
-        dmf = DarkMatterFlux(flavor='nu_mu', channel='w', mass=90)
-        assert dmf is not None
-
-    def test_flux(self):
-        dmf = DarkMatterFlux(flavor='nu_mu', channel='w', mass=90)
-        dmf.get([3])
-        dmf.get([20])
-        with self.assertRaises(ValueError):
-            dmf.get([99])
-        dmf.get([18])
-        energy = np.array([3, 20, 18, 2, 4, 50, 28])
-        dmf.get(energy)
-        bad_energy = np.array([3, 20, 18, 2, 4, 50, 28, 99])
-        with self.assertRaises(ValueError):
-            dmf.get(bad_energy)
