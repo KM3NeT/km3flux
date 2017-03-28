e22e4ac78ce384d9aa4bbec733fcc6b5eb14c129
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-28 10:22:54 +0200

    introduce flux baseclass

--- km3flux/flux.py
+++ km3flux/flux.py
@@ -1,74 +1,113 @@
 """Assorted Fluxes, in  (m^2 sec sr GeV)^-1"""
 
+import logging
+
 import h5py
 import numpy as np
+from scipy.integrade import romberg, simps
 
 from km3flux.data import (HONDAFILE, dm_gc_spectrum, DM_FLAVORS, DM_CHANNELS,
                           DM_MASSES)
 
 
-class Honda2015(object):
+class BaseFlux(object):
+    """Base class for fluxes.
+
+    Methods
+    =======
+    __call__(energy, zenith=None)
+        Return the flux on energy, optionally on zenith.
+    integrate(zenith=None, emin=1, emax=100, **integargs)
+        Integrate the flux via romberg integration.
+    integrate_samples(energy, zenith=None, emin=1, emax=100)
+        Integrate the flux from given samples, via simpson integration.
+
+    Example
+    =======
+    >>> zen = np.linspace(0, np.pi, 5)
+    >>> ene = np.logspace(0, 2, 5)
+
+    >>> from km3flux.flux import MyFlux
+    >>> flux = MyFlux(flavor='nu_mu')
+
+    >>> flux(ene)
+    array([6.68440000e+01, 1.83370000e+01, 4.96390000e+00,
+           1.61780000e+00, 5.05350000e-01,])
+    >>> flux(ene, zen)
+    array([2.29920000e-01, 2.34160000e-02, 2.99460000e-03,
+           3.77690000e-04, 6.87310000e-05])
+    """
+    def __init__(self, **kwargs):
+        raise NotImplementedError
+
+    def __call__(self, energy, zenith=None):
+        if zenith is None:
+            return self._averaged(energy)
+        return self._with_zenith(energy, zenith)
+
+    def _averaged(self, energy):
+        raise NotImplementedError
+
+    def _with_zenith(self, energy, zenith):
+        raise NotImplementedError
+
+    def integrate(self, zenith=None, emin=1, emax=100, **integargs):
+        return romberg(self, emin, emax, vec_fun=True, **integargs)
+
+    def integrate_samples(self, energy, zenith=None, emin=1, emax=100,
+                          **integargs):
+        mask = (emin <= energy) & (energy <= emax)
+        energy = energy[mask]
+        if zenith:
+            zenith = zenith[mask]
+        flux = self(energy, zenith=zenith)
+        return simps(flux, energy, **integargs)
+
+
+class PowerlawFlux(BaseFlux):
+    """E^-gamma flux."""
+    def __init__(self, gamma=2, scale=1e-4):
+        self.gamma = gamma
+        self.scale = scale
+
+    def _averaged(self, energy):
+        return self.scale * np.power(energy, -1 * self.gamma)
+
+    def integrate(self, zenith=None, emin=1, emax=100, **integargs):
+        """Compute analytic integral instead of numeric one."""
+        if np.around(self.gamma, decimals=1) == 1.0:
+            return np.log(emax) - np.log(emin)
+        num = np.power(emax, 1 - self.gamma) - np.power(emin, 1 - self.gamma)
+        den = 1.0 - self.gamma
+        return self.scale * (num / den)
+
+
+class Honda2015(BaseFlux):
     """
     Get Honda 2015 atmospheric neutrino fluxes.
-
-    >>> from km3flux import Honda2015
-    >>> flux = Honda2015()
-
-    >>> zen = np.linspace(0, np.pi, 11)
-    >>> ene = np.logspace(0, 2, 11)
-
-    >>> flux('nu_e_bar', zen, ene)
-    array([  6.68440000e+01,   1.83370000e+01,   4.96390000e+00,
-         1.61780000e+00,   5.05350000e-01,   2.29920000e-01,
-         2.34160000e-02,   2.99460000e-03,   3.77690000e-04,
-         6.87310000e-05,   1.42550000e-05])
-
     """
-    def __init__(self, filename=None):
-        self.tables = {}
-        self.avtables = {}
-        if filename is None:
-            filename = HONDAFILE
+    def __init__(self, flavor='nu_mu'):
+        self.table = None
+        self.avtable = None
+        filename = HONDAFILE
+        if flavor not in ('nu_e', 'anu_e', 'nu_mu', 'anu_mu'):
+            raise ValueError("Unsupported flux '{}'".format(flavor))
         with h5py.File(filename, 'r') as h5:
             self.energy_bins = h5['energy_binlims'][:]
             self.cos_zen_bins = h5['cos_zen_binlims'][:]
-            for flavor in ('nu_e', 'anu_e', 'nu_mu', 'anu_mu'):
-                self.tables[flavor] = h5[flavor][:]
-                self.avtables[flavor] = h5['averaged/' + flavor][:]
+            self.table = h5[flavor][:]
+            self.avtable = h5['averaged/' + flavor][:]
         # adjust upper bin for the case zenith==0
         self.cos_zen_bins[-1] += 0.00001
 
-        # optionally, this can be fixed to make it a single-arg callable
-        self._flavor = None
-
-    @property
-    def flavor(self):
-        return self._flavor
-
-    @flavor.setter
-    def flavor(self, flav):
-        self._flavor = flav
-
-    def __call__(self, energy):
-        # for use e.g. with `scipy.integrate`
-        if self.flavor is None:
-            raise ValueError("No flavor set! aborting...")
-        return self.get(self.flavor, energy)
-
-    def get(self, flavor, energy, zenith=None):
-        if zenith is None:
-            return self._averaged(flavor, energy)
-        else:
-            return self._with_zenith(flavor, energy=energy, zenith=zenith)
-
-    def _averaged(self, flavor, energy):
-        fluxtable = self.avtables[flavor]
+    def _averaged(self, energy):
+        fluxtable = self.avtable
         ene_bin = np.digitize(energy, self.energy_bins)
         ene_bin = ene_bin - 1
         return fluxtable[ene_bin]
 
-    def _with_zenith(self, flavor, energy, zenith):
-        fluxtable = self.tables[flavor]
+    def _with_zenith(self, energy, zenith):
+        fluxtable = self.table
         cos_zen = np.cos(zenith)
         ene_bin = np.digitize(energy, self.energy_bins)
         zen_bin = np.digitize(cos_zen, self.cos_zen_bins)
@@ -77,45 +116,28 @@ class Honda2015(object):
         return fluxtable[zen_bin, ene_bin]
 
 
-class HondaSarcevic(object):
+class HondaSarcevic(BaseFlux):
     """
     Get Honda + Sarcevic atmospheric neutrino fluxes.
     """
-    def __init__(self, filename=None):
-        self.tables = {}
-        self.avtables = {}
-        if filename is None:
-            filename = HONDAFILE
+    def __init__(self, flavor='nu_mu'):
+        self.table = None
+        self.avtable = None
+        filename = HONDAFILE
+        if flavor not in ('nu_e', 'anu_e', 'nu_mu', 'anu_mu'):
+            raise ValueError("Unsupported flux '{}'".format(flavor))
         with h5py.File(filename, 'r') as h5:
             self.energy_bins = h5['energy_binlims'][:]
             self.cos_zen_bins = h5['cos_zen_binlims'][:]
-            for flavor in ('nu_e', 'anu_e', 'nu_mu', 'anu_mu'):
-                self.tables[flavor] = h5['honda_sarcevic/' + flavor][:]
+            self.table = h5['honda_sarcevic/' + flavor][:]
         # adjust upper bin for the case zenith==0
         self.cos_zen_bins[-1] += 0.00001
 
-        # optionally, this can be fixed to make it a single-arg callable
-        self._flavor = None
+    def _averaged(self, energy):
+        raise NotImplementedError("Supports only zenith dependent flux!")
 
-    @property
-    def flavor(self):
-        return self._flavor
-
-    @flavor.setter
-    def flavor(self, flav):
-        self._flavor = flav
-
-    def __call__(self, energy):
-        # for use e.g. with `scipy.integrate`
-        if self.flavor is None:
-            raise ValueError("No flavor set! aborting...")
-        return self.get(self.flavor, energy)
-
-    def get(self, flavor, energy, zenith):
-        return self._with_zenith(flavor, energy=energy, zenith=zenith)
-
-    def _with_zenith(self, flavor, energy, zenith):
-        fluxtable = self.tables[flavor]
+    def _with_zenith(self, energy, zenith):
+        fluxtable = self.table
         cos_zen = np.cos(zenith)
         ene_bin = np.digitize(energy, self.energy_bins)
         zen_bin = np.digitize(cos_zen, self.cos_zen_bins)
@@ -124,12 +146,7 @@ class HondaSarcevic(object):
         return fluxtable[zen_bin, ene_bin]
 
 
-def e2flux(energy, scale=1e-4):
-    """E^-2 spectrum."""
-    return scale * np.power(energy, -2)
-
-
-class DarkMatterFlux(object):
+class DarkMatterFlux(BaseFlux):
     """
     Get Dark Matter spectra (taken from M. Cirelli).
 
@@ -152,12 +169,6 @@ class DarkMatterFlux(object):
         self.counts, self.lims = dm_gc_spectrum(flavor=flavor, channel=channel,
                                                 mass=mass, full_lims=True)
 
-    def __call__(self, energy):
-        return self.get(energy)
-
-    def get(self, energy):
-        return self._averaged(energy)
-
     def _averaged(self, energy):
         energy = np.atleast_1d(energy)
         if np.any(energy > self.lims.max()):
@@ -168,11 +179,17 @@ class DarkMatterFlux(object):
         ene_bin = ene_bin - 1
         return fluxtable[ene_bin]
 
+    def _with_zenith(self, energy, zenith):
+        logging.warning('No zenith dependent flux implemented! '
+                        'Falling back to averaged flux.'
+                       )
+        return self._averaged(self, energy)
+
 
 def dmflux(energy, **kwargs):
     """Get the DM flux for your energies."""
     dmf = DarkMatterFlux(**kwargs)
-    return dmf.get(energy)
+    return dmf(energy)
 
 
 def all_dmfluxes():
